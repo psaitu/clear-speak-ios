5025cadbd156a399122546da55ef490bd26e7590
Prabhu Saitu <prabhusaitu@prabhus-macbook-pro.local>
2019-02-20 12:10:35 -0800

    Initial Commit

---
+++ clear-speak-iosTests/clear_speak_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  clear_speak_iosTests.swift
+//  clear-speak-iosTests
+//
+//  Created by Prabhu Saitu on 2/20/19.
+//  Copyright © 2019 Prabhu Saitu. All rights reserved.
+//
+
+import XCTest
+@testable import clear_speak_ios
+
+class clear_speak_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
