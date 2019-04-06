a7490104b059738413b69a16cc5143894b6895c6
Yunkai Sun <yunkaisun@Yunkais-MacBook-Air.local>
2019-04-06 16:52:59 -0700

    add clear speak UI element project

---
+++ clear_speak_UI/ClearSpeak/ClearSpeakTests/ClearSpeakTests.swift
@@ -0,0 +1,34 @@
+//
+//  ClearSpeakTests.swift
+//  ClearSpeakTests
+//
+//  Created by Yunkai Sun on 4/6/19.
+//  Copyright © 2019 daniel.sun. All rights reserved.
+//
+
+import XCTest
+@testable import ClearSpeak
+
+class ClearSpeakTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
