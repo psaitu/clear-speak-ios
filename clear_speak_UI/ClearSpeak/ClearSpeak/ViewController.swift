631bf8257ee5db55057e67d96371a74fdca3ecde
Yunkai Sun <yunkaisun@Yunkais-MacBook-Air.local>
2019-04-15 00:42:58 -0700

    changed background

--- clear_speak_UI/ClearSpeak/ClearSpeak/ViewController.swift
+++ clear_speak_UI/ClearSpeak/ClearSpeak/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signInBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        signInBtn.backgroundColor = UIColor.gray
+        signInBtn.layer.cornerRadius = 7
+        signInBtn.setTitleColor(UIColor.white, for: .normal)
+        
+        signInBtn.layer.shadowColor = UIColor.gray.cgColor
+        signInBtn.layer.shadowRadius = 6
+        signInBtn.layer.shadowOpacity = 0.5
+        signInBtn.layer.shadowOffset = CGSize(width: 0, height: 0)
+        
+        
     }
 
 
